7a0a08438aee89b86fef97706ace870f2e7ddbe9
Mikael Frosthage <mikael.frosthage@zimpler.com>
2023-12-09 12:58:31 +0100

    day 6 and 7

---
+++ day06/Program.fs
@@ -0,0 +1,60 @@
+﻿
+
+let example = [|
+    (7L,9L);
+    (15L,40L);
+    (30L,200L)
+|]
+
+let input = [|
+     (57L, 291L);
+     (72L, 1172L);
+     (69L, 1176L);
+     (92L, 2026L)
+|]
+
+let input2 = [|
+     (57726992L, 291117211762026L)
+|]
+
+
+
+
+let rec infiniteIntegers n = 
+    seq {
+        yield n
+        yield! infiniteIntegers (n + 1)
+    }
+
+let isRecord (race: int64*int64) (hold:int64) =
+    let timeToDrive = fst race - hold
+    timeToDrive * hold
+
+
+let part1 = input
+           |> Seq.map (fun x->
+                infiniteIntegers 1 
+                |> Seq.map (fun y -> isRecord x y)
+                |> Seq.takeWhile (fun y -> y > 0)
+                |> Seq.toArray
+                |> Array.filter (fun y -> y >= snd x)
+                |> Array.length
+                )
+           |> Seq.reduce (*)
+
+let part2 = input2
+           |> Seq.map (fun x->
+                infiniteIntegers 1 
+                |> Seq.map (fun y -> isRecord x y)
+                |> Seq.takeWhile (fun y -> y > 0)
+                |> Seq.toArray
+                |> Array.filter (fun y -> y >= snd x)
+                |> Array.length
+                )
+           |> Seq.reduce (*)
+
+
+          
+printfn $"part1: %A{part1}\n"
+printfn $"part2: %A{part2}\n"
+            
---
+++ day07/Program.fs
@@ -0,0 +1,174 @@
+﻿
+
+open System.IO
+
+type HandStrength =
+    | FiveOfAKind 
+    | FourOfAKind
+    | FullHouse
+    | ThreeOfAKind
+    | TwoPair
+    | OnePair
+    | HighCard
+    
+type CardAndBid = {
+    hand: string
+    bid: int
+}
+
+let input = File.ReadAllLines("input")
+
+let parse (s: string): CardAndBid =
+    let parts = s.Split(' ')
+    { hand = parts.[0]; bid = int parts.[1] }
+
+let toStrength (c: CardAndBid): HandStrength =
+    let cards = c.hand |> Seq.groupBy id
+    let counts = cards |> Seq.length
+    
+    match counts with
+    | 1 -> FiveOfAKind
+    | 2 ->
+        match Seq.head cards |> snd |> Seq.length with
+        | 4 -> FourOfAKind
+        | 1 -> FourOfAKind
+        | 3 -> FullHouse
+        | 2 -> FullHouse
+        | _ -> failwith "Invalid hand"
+    | 3 ->
+        match cards |> Seq.map (fun (_, v) -> v |> Seq.length) |> Seq.max with
+        | 3 -> ThreeOfAKind
+        | 2 -> TwoPair
+        | _ -> failwith "invalid hand"
+    | 4 -> OnePair
+    | 5 -> HighCard
+    | _ -> failwith "Invalid hand"
+
+let typeValue (h: HandStrength): int =
+    match h with
+    | FiveOfAKind -> 7
+    | FourOfAKind -> 6
+    | FullHouse -> 5
+    | ThreeOfAKind -> 4
+    | TwoPair -> 3
+    | OnePair -> 2
+    | HighCard -> 1
+    
+
+let cardStrength (c: char): int =
+    match c with
+    | 'A' -> 14
+    | 'K' -> 13
+    | 'Q' -> 12
+    | 'J' -> 11
+    | 'T' -> 10
+    | _ -> int c - int '0'
+    
+
+let handComparer(c: CardAndBid) (c2: CardAndBid) =
+    let h1 = c |> toStrength |> typeValue
+    let h2 = c2 |> toStrength |> typeValue
+    
+    if h1 = h2 then
+        let value = Array.zip (c.hand.ToCharArray()) (c2.hand.ToCharArray())
+                  |> Array.map(fun (c1, c2) -> cardStrength c1 - cardStrength c2)
+                  |> Array.filter(fun x -> x <> 0)
+                  |> Array.head
+        value               
+    else
+        h1 - h2
+        
+    
+let part1 = input
+          |> Array.map parse
+          |> Array.sortWith handComparer
+          |> Array.mapi (fun i c -> (i + 1) * c.bid)
+          |> Array.sum
+          
+          
+printfn "%d" part1
+
+let toStrength2 (c: CardAndBid): HandStrength =
+    let cards = c.hand |> Seq.groupBy id
+    let counts = cards |> Seq.length
+    
+    
+    let strength = match counts with
+                    | 1 -> FiveOfAKind
+                    | 2 ->
+                        match Seq.head cards |> snd |> Seq.length with
+                        | 4 -> FourOfAKind
+                        | 1 -> FourOfAKind
+                        | 3 -> FullHouse
+                        | 2 -> FullHouse
+                        | _ -> failwith "Invalid hand"
+                    | 3 ->
+                        match cards |> Seq.map (fun (_, v) -> v |> Seq.length) |> Seq.max with
+                        | 3 -> ThreeOfAKind
+                        | 2 -> TwoPair
+                        | _ -> failwith "invalid hand"
+                    | 4 -> OnePair
+                    | 5 -> HighCard
+                    | _ -> failwith "Invalid hand"
+    let js = cards |> Seq.tryFind (fun (c, v) -> c = 'J') |> Option.map (fun (_, v) -> v |> Seq.length) |> Option.defaultValue 0
+    
+    match (strength, js) with
+    //| FiveOfAKind, 0 -> FiveOfAKind
+    | FiveOfAKind, 5 -> FiveOfAKind
+    | FourOfAKind, 1 -> FiveOfAKind
+    | FourOfAKind, 4 -> FiveOfAKind
+    | FullHouse, 2 -> FiveOfAKind
+    | FullHouse, 3 -> FiveOfAKind
+    | ThreeOfAKind, 1 -> FourOfAKind
+    | ThreeOfAKind, 3 -> FourOfAKind
+    | TwoPair, 2 -> FourOfAKind
+    | TwoPair, 1 -> FullHouse
+    | OnePair, 1 -> ThreeOfAKind
+    | OnePair, 2 -> ThreeOfAKind
+    | HighCard, 1 -> OnePair
+    | x, 0 -> x
+    | x,y  -> failwith $"{c.hand} {x} {y} "
+    
+let cardStrength2 (c: char): int =
+    match c with
+    | 'A' -> 14
+    | 'K' -> 13
+    | 'Q' -> 12
+    | 'J' -> -1
+    | 'T' -> 10
+    | _ -> int c - int '0'
+
+let handComparer2(c: CardAndBid) (c2: CardAndBid) =
+    let h1 = c |> toStrength2 |> typeValue
+    let h2 = c2 |> toStrength2 |> typeValue
+    
+    try 
+        if h1 = h2 then
+            let value = Array.zip (c.hand.ToCharArray()) (c2.hand.ToCharArray())
+                      |> Array.map(fun (c1, c2) -> cardStrength2 c1 - cardStrength2 c2)
+                      |> Array.filter(fun x -> x <> 0)
+                      |> Array.tryHead
+            value |> Option.defaultValue 0
+        else
+            h1 - h2
+    with
+    | :? System.Exception as ex -> 
+        printfn "%A %A" c c2
+        failwith "sdfsfd"
+    
+
+
+let part2 = input
+          |> Array.map parse
+          |> Array.sortWith handComparer2
+          |> Array.mapi (fun i c -> (i + 1) * c.bid)
+          |> Array.sum
+
+          
+printf "%A" part2
+
+
+
+
+
+
